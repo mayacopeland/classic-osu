66737d1dbd5e3460aab6b4da167e179ce7f49d78
Alicia <contact@ilyt.cc>
2019-03-21 22:28:05 +0000

    Fix submission error as a result of the last score not existing

--- handlers/osu_submit.js
+++ handlers/osu_submit.js
@@ -6,7 +6,7 @@ async function handle(req, res) {
     let scoreData = req.query.score.split(":");
     let passed;
     let perfect;
-    console.log(req.files)
+    
     if (scoreData[14] == "True") {
         passed = 1;
     } else {
@@ -22,14 +22,14 @@ async function handle(req, res) {
     let score = new scoreClass(scoreData, passwordMD5, passed, perfect);
     try {
         let scoreExists = await query("SELECT * FROM scores WHERE submit_hash = ?", score.submitHash);
-        if (scoreExists.length > 0) {
+        if (scoreExists[0]) {
             res.end("err: score exists")
             return;
         }
         let userid = await query("SELECT * FROM users WHERE username = ? AND password = ?", score.username, score.password)
         userid = userid[0].id;
         let lastTopScore = await query("SELECT * from SCORES where beatmap_md5 = ? AND userid = ? AND passed = 1", score.mapMD5, score.userid);
-        if (lastTopScore[0].score < score.score) {
+        if (lastTopScore[0].score < score.score || lastTopScore == undefined) {
             await query("UPDATE scores SET passed = 0 WHERE submit_hash = ?". lastTopScore[0].submitHash);
             await query("INSERT INTO scores(userid,submit_hash,beatmap_md5,count300,count100,count50,countGeki,countKatu,misses,score,maxcombo,perfect,mods,passed,rank) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", userid, score.submitHash, score.mapMD5, score.count300, score.count100, score.count50, score.countGeki, score.countKatu, score.countMiss, score.score, score.maxcombo, score.perfect, score.mods, score.passed, score.grade)
             await updateUserScore(userid);
